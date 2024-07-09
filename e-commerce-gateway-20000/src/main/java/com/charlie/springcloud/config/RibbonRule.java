40f6360ee7a0fa5d995e50d3b595c60ed61f765e
charlie <2295509496@qq.com>
2024-07-09 22:03:58 +0800

    Gateway动态路由

---
+++ e-commerce-gateway-20000/src/main/java/com/charlie/springcloud/config/RibbonRule.java
@@ -0,0 +1,19 @@
+//package com.charlie.springcloud.config;
+//
+//import com.netflix.loadbalancer.IRule;
+//import com.netflix.loadbalancer.RandomRule;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * RibbonRule：配置类-配置自己的负载均衡算法
+// */
+//@Configuration
+//public class RibbonRule {
+//
+//    @Bean
+//    public IRule myRibbonRule() {
+//        return new RandomRule();
+//    }
+//
+//}
